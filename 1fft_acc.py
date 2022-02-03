422d0379cda4ff58ea45dc916abd7c29e15a2afc
Alessandro Zanoli <alessandro.zanoli@studenti.unipd.it>
2022-02-03 15:44:24 +0100

    Altre cose

--- 1fft_acc.py
+++ 1fft_acc.py
@@ -1,24 +1,20 @@
 import numpy as np
-import pandas as pd 
 import matplotlib.pyplot as plt 
+from scipy import fft 
+import pandas as pd
 
 sampling_rate = 200 # should be 200 Hz from README1.txt
 df=pd.read_csv("center_sternum.txt",sep=None,engine='python')
 df['time_s'] = np.arange(0,df.shape[0])/100 # let's construct the time axis, with each sampling separated by a 100th of a second (because sampling rate is 100 Hz)
 df=df.set_index('time_s')
 
-acc=df.loc[0:20][['AccX','AccY','AccZ']].values
-print(acc.shape)
-print(df.columns)
-
-ax= plt.subplot(projection='3d')
-ax.scatter(acc[:,0],acc[:,1],acc[:,2],c=df.loc[0:20].index)
+# data for first time window
+df1=df.loc[0:12][['AccX','AccY','AccZ','GyroX','GyroY','GyroZ','MagnX','MagnY','MagnZ']]
+df1.plot(y=['AccX'])
 plt.show()
 
-ax = plt.subplot()
-df.plot(y=['AccX','AccY','AccZ'],ax=ax)
-plt.show()
+accx=df1['AccX'].values
+fft_accx=fft.fft(accx)
 
-ax = plt.subplot()
-df.plot(y=['GyroX','GyroY','GyroZ'],ax=ax)
+plt.plot(fft_accx)
 plt.show()
